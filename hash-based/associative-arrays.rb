5a960db2c92999c128142673b29218213cd3e0fa
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-11-06 16:33:10 +0000

    Add Associative Arrays (Hash)

---
+++ hash-based/associative-arrays.rb
@@ -0,0 +1,18 @@
+require 'awesome_print'
+
+options = {
+  :font_size => 12,
+  :font_family => 'Arial'
+}
+
+states = {
+  AK: 'Alaska',
+  AL: 'Alabama',
+  AR: 'Arkansas',
+  AZ: 'Arizona',
+  CA: 'California',
+  CO: 'Colorado'
+}
+
+ap options
+ap states
