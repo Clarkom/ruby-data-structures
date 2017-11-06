1b7f7cea0c63e0ed9394a2f5f65684de636fcd2a
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-11-06 13:17:56 +0000

    Add Built-in Ruby Search

---
+++ advanced-arrays/built-in-search-behavior.rb
@@ -0,0 +1,12 @@
+names = [
+  "John",
+  "Elizabeth",
+  "Sarah"
+]
+result = names.index("Sarah")
+
+if result
+  puts "The value is located at position #{result}"
+else
+  puts "The value was not found."
+end
